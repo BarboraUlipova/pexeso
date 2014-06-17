ea47ae77981a058e69de795066d159035d564fd9
Barbora Ulipova <b.ulipova@gmail.com>
2014-06-17 18:53:35 +0200

    vypisuje tabulku s otazniky

--- rozhrani.py
+++ rozhrani.py
@@ -1,57 +1,31 @@
 # cmd - konsole
+import pexeso
 import flask
+import os
 
 app = flask.Flask(__name__) 
 
+jmeno_souboru="soubor_s_hrou"
+
 @app.route('/')
 def hello():
-    return """
-    <html>
-        <head>
-             <title>Ahoj!</title>
-        </head>
-        <body>
-             <h1>Ahoj svete!...</h1>
-                <p>
-                  <a href="neco">
-                     Klikni sem!
-                  </a>
-                  nebo:
-                  <a href="https://www.python.org/downloads/release/python-340/">
-                     Sem!
-                  </a>
-                </p>
-                <p>
-                   <img src="obrazek">
-                </p>
-                <form method="POST" action="ahoj">
-                   Jak se jmenujes?
-                   <input type="text" name="jmeno">
-                   <input type="submit" value="JEN SI KLIKNI :)">
-                </form>
-        </body>
-    </html>
-    """
+	if os.path.exists(jmeno_souboru):
+		hra = pexeso.nacti_hru_ze_souboru(jmeno_souboru)
+	else:
+		hra = pexeso.vytvor_hru(pexeso.zamichej_karty())
+		
+	
+	odpoved = []
+	odpoved.append("<table>")
+	for radek in hra['stav']:
+		odpoved.append("  <tr>")
+		for sloupec in radek:
+			odpoved.append("    <td>?</td>")
+		odpoved.append("  </tr>")
+	odpoved.append("</table>")
+	
+	return "\n".join(odpoved)
 
-@app.route('/neco')
-def neco():
-    return 'Tady to je!'
-
-@app.route('/ahoj', methods=['POST'])
-
-def ahoj():
-    text = 'Ahoj. {} je hezke jmeno.'
-    jmeno = flask.request.form['jmeno']
-    return text.format(jmeno)
-
-@app.route('/obrazek')
-def obrazek():
-    jmeno_souboru = r'C:\Sallyino\Fotky\kakadu_ruzovy.jpg' # r je tam kvuli spetnym lomitkum
-    with open(jmeno_souboru, 'rb') as f:
-        obsah = f.read()
-    return flask.Response(
-        obsah,
-        mimetype='image/png')
 
 app.run(debug=True)
 
